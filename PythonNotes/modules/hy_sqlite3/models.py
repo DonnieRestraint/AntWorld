c47e27951cb975dd1c46657f6f93f82dba6dd530
hanyue <hanyueui@163.com>
2023-06-23 18:26:56 +0800

    .

--- PythonNotes/modules/hy_sqlite3/models.py
+++ PythonNotes/modules/hy_sqlite3/models.py
@@ -1,3 +1,35 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, VARCHAR, Boolean, DateTime, ForeignKey
+from sqlalchemy import MetaData, Integer, Column, Table, String, create_engine
+from sqlalchemy.orm import sessionmaker, relationship
 
 Models = declarative_base()
+
+
+class db(object):
+    Models = declarative_base()
+
+
+"""表类创建区"""
+
+
+""""""
+
+if __name__ == '__main__':
+    # 数据库地址
+    db = 'sqlite:///{db_path}?check_same_thread=False'.format(db_path="test.db")
+    # sqlalchemy的引擎
+    engine = create_engine(db, echo=True)
+    # 创建表
+    Models.metadata.create_all(engine)
+    # 会话类绑定引擎
+    Session = sessionmaker(bind=engine)
+    # 开启会话实例
+    session = Session()
+    """测试区"""
+
+    """"""
+    # 提交至数据库
+    session.commit()
+    # 关闭会话
+    session.close()
--- PythonNotes/modules/hy_sqlite3/orm.py
+++ PythonNotes/modules/hy_sqlite3/orm.py
@@ -1,22 +1,18 @@
-import sqlalchemy
 from sqlalchemy import MetaData, Integer, Column, Table, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-models = declarative_base()
+
 class SqliteDb(object):
     debug = True
     echo = True if debug else False
 
-    def __init__(self, db_path):
-        self.sqlite_url = 'sqlite:///{db_path}?check_same_thread=False'.format(db_path="foo.db")
-        self.engine = create_engine(self.sqlite_url, self.echo)
-        self.Session = sessionmaker(bind=self.engine)
+    def __init__(self, db_path='test.db'):
+        db = 'sqlite:///{db_path}?check_same_thread=False'.format(db_path=db_path)
+        self.engine = create_engine(db, echo=self.echo)
 
-    def create_table(self, table_name, *args):
+    def create_table(self, table_name, **kwargs):
         meta = MetaData()
-        Column('id', Integer, primary_key=True),
-        columns = [Column(str(arg), String(32)) for arg in args]
+        columns = [Column(str(field_name), field_type) for field_name, field_type in kwargs.items()]
         table = Table(
             table_name,
             meta,
@@ -24,15 +20,61 @@ class SqliteDb(object):
             *columns
         )
         meta.create_all(self.engine)
+        return table
 
-    def delete(self, table, *args):
-        pass
+    def exe_sql(self, sql, args=None):
+        with self.engine.connect() as con:
+            try:
+                # 'select * from persons'
+                results = con.execute(sql, args)
+                for result in results:
+                    print(result)
+            except Exception as err:
+                print(err)
 
-    def read(self):
-        pass
+    def read(self, class_name, condition):
+        session = self.get_session()
+        # where = (class_name.name == 'ed')
+        results = session.query(class_name).filter(condition).first()
+        return results
 
-    def update(self):
-        pass
+    def delete(self, table, **kwargs):
+        session = self.get_session()
+        try:
+            del_user = session.query(table).filter_by(**kwargs).first()
+            # 将ed用户记录删除
+            session.delete(del_user)
+            session.commit()
+        except Exception as err:
+            # session.rollback()
+            print(err)
 
-    def create(self):
-        pass
+    def update(self, class_name, where, **kwargs):
+        session = self.get_session()
+        try:
+            where = (class_name.name == 'ed')
+            result = session.query(class_name).filter_by(where).first()
+            for key, val in kwargs.items():
+                setattr(result, key, val)
+        except Exception as err:
+            session.rollback()
+            print(err)
+
+    def create(self, *args):
+        session = self.get_session()
+        try:
+            session.add_all(args)
+            session.commit()
+        except Exception as err:
+            session.rollback()
+            print(err)
+
+    def get_session(self):
+        return sessionmaker(bind=self.engine)()
+
+
+if __name__ == '__main__':
+    print("http://www.noobyard.com/article/p-ejbggzgx-c.html")
+    print("https://blog.csdn.net/m0_57145438/article/details/130874157")
+    print("https://blog.csdn.net/qq_41741971/article/details/126217235")
+
